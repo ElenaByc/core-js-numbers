ce4082f09215cff53d8eeab8e2a88b401f2897a9
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-11-14 21:27:29 -0800

    feat: isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -199,8 +199,17 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  const maxNum = Math.sqrt(n);
+  for (let i = 2; i <= maxNum; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
